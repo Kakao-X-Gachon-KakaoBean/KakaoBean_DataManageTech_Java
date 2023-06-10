219a73672a813b1e0acfcd0602c423d61cd33ef8
happysubin <asb0711@gmail.com>
2023-06-10 20:23:39 +0900

    fix: 카프카 토픽 전송 기능 수정

--- src/main/java/com/kakaobean/dataTech/service/KafkaService.java
+++ src/main/java/com/kakaobean/dataTech/service/KafkaService.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakaobean.dataTech.config.KafkaConfig;
 import com.kakaobean.dataTech.domain.SubwayUsage;
+import com.kakaobean.dataTech.domain.dto.Dto;
 import com.kakaobean.dataTech.domain.respositorty.SubwayUsageRepository;
+import com.kakaobean.dataTech.infrastructure.SubwayUsageQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -17,13 +19,13 @@ import java.util.List;
 public class KafkaService {
 
     private final KafkaTemplate kafkaTemplate;
-    private final SubwayUsageRepository subwayUsageRepository;
     private final ObjectMapper objectMapper;
+    private final SubwayUsageQueryRepository queryRepository;
 
     public void sendData() throws JsonProcessingException {
-        List<SubwayUsage> list = subwayUsageRepository.findAll();
-        for (SubwayUsage subwayUsage : list) {
-            kafkaTemplate.send(KafkaConfig.TOPIC_NAME, objectMapper.writeValueAsString(subwayUsage));
+        List<Dto> list = queryRepository.findAll();
+        for (Dto dto : list) {
+            kafkaTemplate.send(KafkaConfig.TOPIC_NAME, objectMapper.writeValueAsString(dto));
         }
     }
 }
